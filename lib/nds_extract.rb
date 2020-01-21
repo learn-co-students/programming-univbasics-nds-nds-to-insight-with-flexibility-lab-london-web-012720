e22700224331b1060f359eeba3151e448a1cbec6
Bryn Heath <brynheath1@gmail.com>
2020-01-21 22:53:53 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,7 +3,7 @@ require 'directors_database'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
-
+###############################################################################
 def flatten_a_o_a(aoa)
   result = []
   i = 0
@@ -16,12 +16,13 @@ def flatten_a_o_a(aoa)
     end
     i += 1
   end
-
+p result
   result
 end
+###############################################################################
 
 def movie_with_director_name(director_name, movie_data)
-  { 
+  {
     :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
@@ -29,43 +30,73 @@ def movie_with_director_name(director_name, movie_data)
     :director_name => director_name
   }
 end
+###############################################################################
 
 
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+  arr = []
+part1 = name
+i=0
+    while i < movies_collection.size do
+        part2 = movies_collection[i]
+        arr << movie_with_director_name(part1,part2)
+        p arr
+        i += 1
+    end
+   return arr
 end
-
+# GOAL: For each Hash in an Array (movies_collection),
+# provide a collection of movies and a directors name to the movie_with_director_name method
+# and accumulate the returned Array of movies into a new Array that's
+# returned by this method.
+#
+# INPUT:
+# * name: A director's name
+# * movies_collection: An Array of Hashes where each Hash represents a movie
+#
+# RETURN:
+#
+# Array of Hashes where each Hash represents a movie; however, they should all have a
+# :director_name key. This addition can be done by using the provided
+# movie_with_director_name method
+###############################################################################
 
 def gross_per_studio(collection)
+pp collection
+result = { }
+puts "***************************"
+i=0
+
+   while i < collection.size do
+
+     if result.has_value?(:studio)
+       result[i][:studio] = collection[i][:studio]
+      else
+       result[collection[i][:studio]] = collection[i][:worldwide_gross]
+     end
+       i += 1
+       puts result
+    end
+ return result
+
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
-  #
   # INPUT:
   # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
   # RETURN:
-  #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
 end
 
+###############################################################################
+
 def movies_with_directors_set(source)
+   use = source[:name]
+   neww = flatten_a_o_a(use)
+
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
@@ -77,6 +108,7 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
 end
+###############################################################################
 
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
